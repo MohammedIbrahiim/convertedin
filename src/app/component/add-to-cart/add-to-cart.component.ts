3527bcde5545ac30e63853c1813ffbfab9ee5f67
mohamedibraahiim <169139555+mohamedibraahiim@users.noreply.github.com>
2024-07-31 13:55:48 +0300

    add total price to cart

--- src/app/component/add-to-cart/add-to-cart.component.ts
+++ src/app/component/add-to-cart/add-to-cart.component.ts
@@ -6,7 +6,7 @@ import { ProductsService } from '../../core/api/products/products.service';
 import { Store } from '@ngrx/store';
 import { ProductState } from '../../feature/state';
 import { selectCartItems } from '../../feature/state/product.selector';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { productAction } from '../../feature/state/product-type';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
@@ -22,30 +22,25 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 export class AddToCartComponent implements OnInit {
   cartItems$: Observable<any>|undefined;
   cart$: Observable<any[]>|undefined;
+  totalPrice$: Observable<number>|undefined;
+  visible= this._ProductsService.visible
+
   constructor(private _ProductsService:ProductsService , private store: Store<{ product: ProductState }>,private __destroyRef: DestroyRef){
     this.cartItems$ = this.store.select(selectCartItems);
+  this.totalPrice$ = this.cartItems$.pipe(
+    map(products => products.reduce((total: any, product: { price: any; }) => total + product.price, 0)),
+    takeUntilDestroyed(__destroyRef)
+  );
 
   }
 
   ngOnInit(): void {
+    this.store.dispatch(productAction.clearCart());
     this.cart$ = this.store.select(selectCartItems);
-
-
-
   }
-  visible= this._ProductsService.visible
   showDialog() {
     this._ProductsService.setVisible(false)
   }
 
-  removeFromCart(productId: number) {
-    this.store.dispatch(productAction.removeFromCart({ productId }));
-    this.cart$?.subscribe((res=>{
-      
-    }))
-        
-  }
-
-  
 
 }
